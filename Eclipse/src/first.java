691ddf861283201bba5edb21ea280bb69a2873e8
RedCatHuu <sato.value@gmail.com>
2024-02-28 23:39:58 +0900

    変数への文字列の代入と出力

--- Eclipse/src/first.java
+++ Eclipse/src/first.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class first {
 
 	public static void main(String[] args) {
 		System.out.println("Eclipseを始めよう");
-
+		
+//		next と　nextLine
+		Scanner sc = new Scanner("a b c");
+		String a = sc.next();
+		String b = sc.next();
+		String c = sc.next();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		sc = new Scanner("a b c");
+		String line = sc.nextLine();
+		System.out.println(line);
 
 	}
 
