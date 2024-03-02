48ec873d4bba03ccd15f6fc29cbe0bf5463bc678
RedCatHuu <sato.value@gmail.com>
2024-03-02 22:58:47 +0900

    [add]変数宣言

--- Eclipse/src/first.java
+++ Eclipse/src/first.java
@@ -25,6 +25,32 @@ public class first {
 		
 		// 配列の宣言
 		int[][] array = {{1, 2}, {3, 4}, {5, 6}};
+		
+		// 浮動小数点の宣言
+		double data = 1.234;
+		System.out.println(data);
+		// 長い浮動小数点はアンダースコアを挿入できる。
+		double data2 = 1.234_567_891;
+		System.out.println(data2); // 1.234567891
+		// double型には整数を入れることもできる。自動型変換。
+		double data3 = 0.1;
+		data3 = 100;
+		System.out.println(data3); // 100.0
+		
+		// 文字の宣言(シングルクオーテーション)
+		char ch = 'a';
+		System.out.println(ch);
+		// char型はint型の変数に代入できる。自動型変換。文字番号が表示される。
+		int number = ch;
+		System.out.println(number); //97
+		
+		// 文字列の宣言
+		String str = "文字列";
+		System.out.println(str);
+		
+		// 論理値
+		boolean result = true;		
+		System.out.println(result);
 
 		// for文　jsと違うのは宣言部分だけかな。
 		for (int i = 0; i < 3; i++) {
