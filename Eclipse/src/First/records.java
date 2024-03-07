56b5a5589fc50672060122a6fd3eb8e8293728ed
RedCatHuu <sato.value@gmail.com>
2024-03-07 23:18:19 +0900

    [add]文字列配列

--- Eclipse/src/First/records.java
+++ Eclipse/src/First/records.java
@@ -1,5 +1,7 @@
 package First;
 
+import java.util.ArrayList;
+
 public class records {
 
 	// オブジェクト(インスタンス)を作成できる。
@@ -12,6 +14,42 @@ public class records {
 		System.out.println(meibo.number()); // 100
 		System.out.println(meibo.name()); // 田中
 		System.out.println(meibo.mail()); // t@t
+		
+		// 可変リスト
+		// 文字列型の配列
+		var list = new ArrayList<String>();
+		System.out.println(list); // []
+		list.add("apple");
+		list.add("banana");
+		list.add("grape");
+		System.out.println(list); // [apple, banana, grape]
+		
+		for(String item : list) {
+			System.out.println(item);
+		}
+		
+		// 要素数
+		System.out.println(list.size()); // 3
+		
+		// 指定した要素の取得
+		System.out.println(list.get(1)); // banana
+		
+		// 指定した要素を含むか判定
+		System.out.println(list.contains("grape")); // true
+		
+		// 指定したインデックスの要素を削除。返り値は削除した要素
+		System.out.println(list.remove(2)); // grape	
+		System.out.println(list); // [apple, banana]
+		
+		// 要素の全削除
+		list.clear();
+		System.out.println(list); // []
+		
+		// 要素が空か判定
+		System.out.println(list.isEmpty()); // true
+		
+		
+		
+		
 	}
-
 }
