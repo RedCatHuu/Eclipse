b34ff3f7bb42f1b76dcf6b32204fae4f4e243849
RedCatHuu <sato.value@gmail.com>
2024-03-22 22:32:54 +0900

    [add]ゲッター・セッター

--- Eclipse/src/First/Tax.java
+++ Eclipse/src/First/Tax.java
@@ -15,8 +15,43 @@ public class Tax {
 		this.shotoku = shotoku;
 		this.koujo = koujo;
 	}
-
+	
+	// ゲッター
 	public int zeigaku() {
 		return (int)(( shotoku - koujo) * 0.1);
 	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getShotoku() {
+		return shotoku;
+	}
+
+	public int getKoujo() {
+		return koujo;
+	}
+	
+	// セッター
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setShotoku(int shotoku) {
+		this.shotoku = shotoku;
+	}
+
+	public void setKoujo(int koujo) {
+		this.koujo = koujo;
+	}
+	
 }
--- Eclipse/src/First/useTax.java
+++ Eclipse/src/First/useTax.java
@@ -6,7 +6,16 @@ public class useTax {
 		// Taxクラスで作成したオブジェクトからインスタンスを作成。
 		Tax tax = new Tax(100, "田中", 300000, 100000);
 		System.out.println(tax.zeigaku() ); // 20000
-
+		
+		System.out.println("納税者番号=" + tax.getNumber());
+		System.out.println("氏名=" + tax.getName());
+		System.out.println("所得額=" + tax.getShotoku());
+		System.out.println("控除額=" + tax.getKoujo()); // 控除額=100000
+		
+		System.out.println("所得税=" + tax.zeigaku());
+		tax.setKoujo(120000);
+		System.out.println("控除額=" + tax.getKoujo()); // 控除額=120000
+		System.out.println(tax.zeigaku()); // 18000
 	}
 
 }
